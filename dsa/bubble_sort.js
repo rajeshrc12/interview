4cacb7eb74ea4c59347852f205199e85ed9c2622
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-04-14 19:12:58 +0530

    dsa: bubble sort

---
+++ dsa/bubble_sort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([1, 22, 4, 6, 23, 67]))
